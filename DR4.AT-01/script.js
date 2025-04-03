d1e0d12de2dad2e3035385fe1e2e9404b5e1c09a
Lohran Mendes <lohranfm@gmail.com>
2025-04-03 20:55:53 -0300

    feat: add array lib; solved question 1

--- DR4.AT-01/script.js
+++ DR4.AT-01/script.js
@@ -1,3 +1,4 @@
+// Parte 1. Escrever programas em JavaScript com arrays e encontrar e tratar bugs (erros) em JavaScript
 // 1 Elemento mais frequente em um array
 
 // Enunciado: Crie uma página com um programa JavaScript para encontrar o item mais frequente em um array.
@@ -10,3 +11,25 @@
 // Considerações:
 
 // O array original poderá ser alterado para testar outros cenários de execução.
+
+import { elementoMaisFrequente } from "../lib/array.js";
+
+const array = [
+  "naruto",
+  "sakura",
+  "sasuke",
+  "ino",
+  "kakashi",
+  "gaara",
+  "naruto",
+  "sakura",
+  "sasuke",
+  "jiraya",
+  "naruto",
+];
+document.body.innerHTML += `<p>Aqui está o array informado: [${array.join(
+  ", "
+)}]</p>`;
+document.body.innerHTML += `<p>O item do array que aparece com mais frequência é [${
+  elementoMaisFrequente(array)[0].join(', ')
+}] aparecendo ${elementoMaisFrequente(array)[1]} vezes.</p>`;
