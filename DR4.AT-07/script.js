a4738f737046c7797f2877e3a7361f428b15140e
Lohran Mendes <lohranfm@gmail.com>
2025-04-04 16:31:21 -0300

    feat: solved question 7

--- DR4.AT-07/script.js
+++ DR4.AT-07/script.js
@@ -9,3 +9,77 @@
 // Use prompt e alert.
 // Coloque pelo menos 5 questões.
 // Teste o seu programa com cenários diferentes.
+
+import { recebeString } from "../lib/string.js";
+
+const comoAvaliar =
+  "Responda somente com:\n'Concordo', 'Concordo Parcialmente' ou 'Discordo'\n\r";
+const perguntas = [
+  "Fui bem atendido em todo o momento.",
+  "Consegui tudo o que eu queria.",
+  "O valor do serviço foi muito justo.",
+  "O atendimento foi bem rápido.",
+  "Vou recomendar os serviços para meus conhecidos.",
+];
+const respostas = [];
+let resposta = "";
+
+function recebeAvaliacao() {
+  for (const pergunta of perguntas) {
+    if (resposta === null) {
+      break;
+    }
+    do {
+      resposta = recebeString(
+        prompt(comoAvaliar + "" + pergunta),
+        "console.log"
+      );
+      if (validaAvaliacao(resposta) && resposta !== null) {
+        respostas.push(resposta.toLowerCase());
+      } else if (validaAvaliacao(resposta) && resposta === null) {
+        alert("Cancelando a pesquisa de satisfação...");
+      } else {
+        alert(
+          `"${resposta}" - não é uma resposta válida! Responda com concordo, concordo plenamente ou discordo`
+        );
+      }
+    } while (validaAvaliacao(resposta) === false);
+  }
+  if (resposta !== null) alert(`Agradecemos pela sua participação!\n\r${percentualDeSatisfacao()}`);
+}
+
+function validaAvaliacao(inputResposta) {
+  let resposta = inputResposta;
+  if (
+    resposta === null ||
+    resposta.toLowerCase() === "concordo" ||
+    resposta.toLowerCase() === "concordo parcialmente" ||
+    resposta.toLowerCase() === "discordo"
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function percentualDeSatisfacao(){
+  let pontuacao = 0;
+  respostas.forEach(resposta =>{
+    switch (resposta) {
+      case 'concordo':
+        pontuacao += 10;
+        break;
+      case 'concordo parcialmente':
+        pontuacao += 5;
+        break;
+      case 'discordo':
+        pontuacao += 0;
+        break;
+    
+      default:
+        break;
+    }
+  })
+ return `Ficamos com uma pontuação de ${pontuacao} em um máximo de ${respostas.length * 10}`;
+}
+
+recebeAvaliacao();
