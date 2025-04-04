fd54036ecf05db92c5c437caca50e2d11274dfd3
Lohran Mendes <lohranfm@gmail.com>
2025-04-04 19:30:18 -0300

    feat: solved question 9

--- DR4.AT-09/script.js
+++ DR4.AT-09/script.js
@@ -7,3 +7,14 @@
 // Use prompt e alert.
 // Use expressões regulares.
 // Teste o seu programa com cenários diferentes.
+
+function ValidaStringNumerica(inputString) {
+  const regex = /^\d+$/;
+  if (regex.test(inputString)) {
+    return alert(`Válido! "${inputString}" só tem valores numéricos`);
+  }
+  alert(`Inválido! "${inputString}" tem valores que não são numéricos`);
+}
+
+const entrada = prompt("Insira a string para que a validação seja feita.");
+ValidaStringNumerica(entrada);
