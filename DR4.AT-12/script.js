add644167bba45235374a552b93922f5cfee91b4
Lohran Mendes <lohranfm@gmail.com>
2025-04-05 17:35:57 -0300

    feat: solved question 12

--- DR4.AT-12/script.js
+++ DR4.AT-12/script.js
@@ -7,3 +7,33 @@
 // Use prompt e alert.
 // Remover todos os caracteres não alfanuméricos (pontuação, espaços e símbolos) e transformar tudo para minúsculo.
 // Teste o seu programa com cenários diferentes.
+
+function verificaPalindromo() {
+  let entrada;
+  try {
+    entrada = prompt(
+      "Insira aqui a frase a ser verificada como palíndromo."
+    ).trim();
+    if (typeof entrada !== "string" || entrada.length === 0) {
+      throw new Error(
+        "O valor de entrada não pode ser vazio!\nTente novamente com um valor válido."
+      );
+    }
+  } catch (error) {
+    alert(error);
+    return;
+  }
+  const texto = entrada
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f\s\-,.;!?]/g, "");
+  const textoDeComparacao = texto.split("").reverse().join("");
+
+  if (texto === textoDeComparacao) {
+    alert("Parabéns! '" + entrada + "' é um palíndromo!");
+    return;
+  }
+  alert("Que pena! '" + entrada + "' não é um palíndromo!");
+}
+
+verificaPalindromo();
