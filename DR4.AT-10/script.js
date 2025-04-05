ef08af799882c9856d8a916aecca21521dfd8a8d
Lohran Mendes <lohranfm@gmail.com>
2025-04-04 23:05:12 -0300

    feat: solved question 10

--- DR4.AT-10/script.js
+++ DR4.AT-10/script.js
@@ -6,3 +6,30 @@
 
 // Use prompt e alert.
 // Teste o seu programa com cenários diferentes.
+
+import { validaString } from "../lib/string.js";
+
+function contagemDeVogais() {
+  const string = prompt(
+    "Insira aqui a string para que a contagem de vogais seja realizada."
+  );
+  let contagemVogal = 0;
+  if (validaString(string, "alert")) {
+    const letters = string.split("");
+    letters.forEach((letter) => {
+      if (
+        letter.toLowerCase() === "a" ||
+        letter.toLowerCase() === "e" ||
+        letter.toLowerCase() === "i" ||
+        letter.toLowerCase() === "o" ||
+        letter.toLowerCase() === "u"
+      )
+        contagemVogal++;
+    });
+    if (contagemVogal > 0) {
+      alert(`Existem ${contagemVogal} vogais no texto fornecido`);
+    } else alert(`Não há vogais no texto fornecido`);
+  }
+}
+
+contagemDeVogais();
