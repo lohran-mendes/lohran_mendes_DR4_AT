c7103d62fa80eae71dc60238f4da6443637c5665
Lohran Mendes <lohranfm@gmail.com>
2025-04-05 18:34:03 -0300

    feat: update question 14

--- DR4.AT-14/script.js
+++ DR4.AT-14/script.js
@@ -7,3 +7,32 @@
 // Use prompt e alert.
 // Dispare um erro caso o nome próprio informado não seja passível de abreviação. Use throw e o erro mais adequado ao tipo de problema encontrado.
 // Teste o seu programa com cenários diferentes.
+
+function abreviaNome(string) {
+  let entradaNome, nomeAbreviado;
+
+  try {
+    if(typeof string === 'string' && string.trim().length > 0){
+    entradaNome = string.trim().toLowerCase().split(' ')
+    if(entradaNome.length < 3) throw new Error("Não é possível abreviar um nome tão curto!");
+    } else {
+      throw new Error("A entrada não pode ser uma string vazia!");
+    }
+  } catch (error) {
+    alert(error)
+  }
+  nomeAbreviado = entradaNome.map(nome => {
+    return nome.replace(nome[0], nome[0].toUpperCase())
+  })
+  for (let i = 1; i < (nomeAbreviado.length - 1); i++) {
+    nomeAbreviado[i] = nomeAbreviado[i].slice(0, 1).concat('.')
+  }
+
+  console.log(entradaNome)
+  console.log(nomeAbreviado)
+
+
+  return nomeAbreviado.join(' ');
+}
+
+console.log(abreviaNome('Lohran fellipe mendes de souza'))
