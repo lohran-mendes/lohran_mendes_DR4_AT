6c9604929dcf00628966c978d557679169f68f93
Lohran Mendes <lohranfm@gmail.com>
2025-04-05 17:01:54 -0300

    feat: solved question 11

--- DR4.AT-11/script.js
+++ DR4.AT-11/script.js
@@ -7,3 +7,50 @@
 // Use prompt e alert.
 // Dispare um erro caso a string informada não seja válida. Use throw e o erro mais adequado ao tipo de problema encontrado.
 // Teste o seu programa com cenários diferentes.
+function confereAnagrama() {
+  let primeiroAnagrama, segundoAnagrama;
+  try {
+    primeiroAnagrama = prompt("Insira aqui a primeira string.");
+    if (
+      typeof primeiroAnagrama !== "string" ||
+      primeiroAnagrama.trim().length === 0
+    ) {
+      throw new Error("O primeiro Anagrama não pode ser um valor vazio.");
+    }
+    segundoAnagrama = prompt("Insira aqui a segunda string.");
+    if (
+      typeof segundoAnagrama !== "string" ||
+      segundoAnagrama.trim().length === 0
+    ) {
+      throw new Error("O segundo Anagrama não pode ser um valor vazio.");
+    }
+  } catch (error) {
+    alert(error);
+    return;
+  }
+
+  const arrayDoAnagrama1 = primeiroAnagrama
+    .toLowerCase()
+    .replaceAll(" ", "")
+    .split("")
+    .sort()
+    .join("");
+  const arrayDoAnagrama2 = segundoAnagrama
+    .toLowerCase()
+    .replaceAll(" ", "")
+    .split("")
+    .sort()
+    .join("");
+
+  if (arrayDoAnagrama1 === arrayDoAnagrama2) {
+    alert(
+      `Correto! "${primeiroAnagrama}" é um anagrama de ${segundoAnagrama}!`
+    );
+    return;
+  }
+  alert(
+    `Errado! "${primeiroAnagrama}" não é um anagrama de ${segundoAnagrama}!`
+  );
+}
+
+confereAnagrama();
