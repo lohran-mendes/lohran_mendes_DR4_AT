25a567fb0670a187aee1e0b124f71b86efdb1cbc
Lohran Mendes <lohranfm@gmail.com>
2025-04-05 20:45:16 -0300

    feat: solved question 14

--- DR4.AT-14/script.js
+++ DR4.AT-14/script.js
@@ -1,6 +1,6 @@
 // 14 Abreviação de nome próprio
 
-// Enunciado: Crie uma página que use uma função para converter uma string com um nome próprio em formato abreviado, como usado aqui no Brasil. Por exemplo: “Pedro Álvares Cabral” ficaria “Pedro A. Cabral”.
+// Enunciado: Crie uma página que use uma função para converter uma string com um nome próprio em formato abreviado, como usado aqui no Brasil. Por exemplo: “Pedro Álvares Cabral” ficaria “Pedro Á. Cabral”.
 
 // Observações:
 
@@ -10,29 +10,33 @@
 
 function abreviaNome(string) {
   let entradaNome, nomeAbreviado;
+  const preposicoesEmNomes = ["de", "da", "do", "das", "dos", "e"];
 
   try {
-    if(typeof string === 'string' && string.trim().length > 0){
-    entradaNome = string.trim().toLowerCase().split(' ')
-    if(entradaNome.length < 3) throw new Error("Não é possível abreviar um nome tão curto!");
+    if (typeof string === "string" && string.trim().length > 0) {
+      entradaNome = string.trim().toLowerCase().split(" ");
+      nomeAbreviado = entradaNome.filter(
+        (palavra) => !preposicoesEmNomes.includes(palavra)
+      );
+      if (nomeAbreviado.length < 3)
+        throw new Error("Não é possível abreviar um nome tão curto!");
     } else {
       throw new Error("A entrada não pode ser uma string vazia!");
     }
   } catch (error) {
-    alert(error)
-  }
-  nomeAbreviado = entradaNome.map(nome => {
-    return nome.replace(nome[0], nome[0].toUpperCase())
-  })
-  for (let i = 1; i < (nomeAbreviado.length - 1); i++) {
-    nomeAbreviado[i] = nomeAbreviado[i].slice(0, 1).concat('.')
+    alert(error);
+    return
   }
 
-  console.log(entradaNome)
-  console.log(nomeAbreviado)
+  for (let i = 1; i < nomeAbreviado.length - 1; i++) {
+    nomeAbreviado[i] = nomeAbreviado[i].slice(0, 1).concat(".");
+  }
+  nomeAbreviado = nomeAbreviado.map((nome) => {
+    return nome.replace(nome[0], nome[0].toUpperCase());
+  });
 
-
-  return nomeAbreviado.join(' ');
+  alert(`O nome abreviado fica: '${nomeAbreviado.join(" ")}'`)
+  return nomeAbreviado.join(" ");
 }
 
-console.log(abreviaNome('Lohran fellipe mendes de souza'))
+abreviaNome(prompt('Insira aqui o nome para saber a sua abreviação.'))
