7d79c585d53cbe0451d4374ff552d9d97f2adb5d
Lohran Mendes <lohranfm@gmail.com>
2025-04-05 21:26:21 -0300

    feat: solved question 15

--- DR4.AT-15/script.js
+++ DR4.AT-15/script.js
@@ -6,3 +6,34 @@
 
 // Use prompt e alert.
 // Teste o seu programa com cenários diferentes.
+
+function truncarString(string, maxStringLength) {
+  try {
+    if (typeof string !== "string" || string.trim().length === 0)
+      throw new Error("A string fornecida não pode ser vazia!");
+    if (
+      typeof maxStringLength !== "number" ||
+      maxStringLength <= 0 ||
+      !Number.isInteger(maxStringLength)
+    )
+      throw new Error(
+        "O tamanho máximo da string deve ser um valor numérico inteiro maior que 0!"
+      );
+  } catch (error) {
+    alert(error);
+    return;
+  }
+  if (string.length >= maxStringLength) {
+    const arrayString = string.slice(0, maxStringLength).split("");
+    alert(
+      arrayString.join("").slice(0, arrayString.lastIndexOf(" ")).concat("...")
+    );
+    return;
+  }
+  alert(string);
+}
+
+truncarString(
+  prompt("Entre com a string que deve ser truncada"),
+  Number(prompt("Entre com o tamanho máximo da string."))
+);
